7ec42779b3062c515c74d19a863bbbc8155b95b3
joaopedrogianini <joaopedrogianinifranzin@gmail.com>
2024-05-21 08:44:35 -0300

    criado componente tasklist

---
+++ src/components/screens/Task_List.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import {StyleSheet, View, Text} from "react-native";
+
+export default class TaskList extends Component{
+    render(){
+        return(
+            <View style = {styles.container}>
+                <Text>Lista de Tarefas</Text>
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    container:{
+        flex: 1
+    }
+})
