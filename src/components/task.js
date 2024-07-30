8d4e16163e262a6c4b5b4fd64a8cbea3d3c4246a
joaopedrogianini <joaopedrogianinifranzin@gmail.com>
2024-07-30 10:39:50 -0300

    aula

--- src/components/task.js
+++ src/components/task.js
@@ -1,62 +1,74 @@
-import React from "react";
-import {Text, View, StyleSheet, TouchableWithoutFeedback} from "react-native"
-import moment from "moment" 
-import {Icon} from "react-native-vector-icons/FontAwesome6"
+import React from "react"
+import { StyleSheet, View, Text, TouchableWithoutFeedback } from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome6"
+import moment from "moment"
+import 'moment/locale/pt-br'
 
-export default props => {
-    return(
-        <View style={StyleSheet.container}>
-        <TouchableWithoutFeedback>
-            <View style={styles.checkContaioner}>
-                <View style={styles.done}>
-                    <Icon name="check" size={20}></Icon>
+export default props => { 
+    const done_or_not = props.done_at != null? {textDecorationLine: "line-through"}: {}
+    const date =props.date_at != null ? props.donet_at : props.estimate_at 
+    const formated_date = moment(date).format('ddd, D [de] MMM')
+    return (
+        <View style={styles.container}>
+            <TouchableWithoutFeedback onPress={()=> alert("Olá")}>
+                <View style={styles.checkContainer}>
+                  {get_Check_View(props.done.at)}
                 </View>
-            </View>
-        </TouchableWithoutFeedback>
+            </TouchableWithoutFeedback>
             <View>
-                <Text style={styles.desc}>{props.description}</Text>
-                <Text style={styles.date}>{props.estimate_at + ""}</Text>
-                <Text style={styles.date}>{props.done_at + ""}</Text>
+                <Text style={[styles.desc, done_or_not]}>{props.description}</Text>
+                <Text style={styles.date}>{formated_date}</Text>
+              
             </View>
         </View>
     )
 }
+function get_Check_View(done_at){
+    if (done_at != null){
+        return(
+        <View style={styles.done}>
+        <Icon name="check" size={20}></Icon>
+    </View>
+        )
+    }else{
+        <View style={styles.pending}>
 
+        </View>
+    }
+}
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flexDirection: "row",
         borderColor: '#AAA',
-        borderBottomWidth: 1, 
-        alignItems: 'center', 
+        borderBottomWidth: 1,
+        alignItems: 'center',
         paddingVertical: 10
-    }, 
-    desc:{
-        fontFamily: "Arial", 
-        fontSize: 20, 
-        color: "#333333"
-    }, 
-    date:{
-        fontFamily: "Arial", 
-        color: "#555555"
-    }, 
-    checkContaioner:{
+    },
+    desc: {
+        fontFamily: 'Arial',
+        fontSize: 20
+    },
+    date: {
+        fontFamily: 'Arial'
+    },
+    checkContainer:{
         width: "20%",
-        alignItems: "center", 
+        alignItems: "center",
         justifyContent: "center"
     },
     done:{
-        height:25,
-        width:25,
-        borderRadius:13,
+        height: 25,
+        width: 25,
+        borderRadius: 13,
         borderWidth:1,
-        backgroundColor:"#4D7031", 
-        alignItems:"center",
+        backgroundColor: "#4D7031",
+        alignItems: "center",
         justifyContent: "center"
     },
     pending:{
-        height:25,
-        width:25,
-        borderRadius:13,
+        height: 25,
+        width: 25,
+        borderRadius: 13,
         borderWidth:1,
         borderColor: "#555555"
     }
